9434eee3b33c969dc60563db70f92914ed07a30a
Tom <tom.wei@onedegree.hk>
2025-01-16 14:39:52 +0800

    [BE] Initialize the repo

---
+++ utils/util.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+class GeneralUtil:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_current_time():
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
