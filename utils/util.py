c466ebbfac163a148ba8f7b95253225c6becb13d
Tom <tom.wei@onedegree.hk>
2025-01-16 15:17:42 +0800

    [BE] Add a new util function

--- utils/util.py
+++ utils/util.py
@@ -8,3 +8,7 @@ class GeneralUtil:
     @staticmethod
     def get_current_time():
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    @staticmethod
+    def get_current_date():
+        return datetime.now().strftime("%Y-%m-%d")
